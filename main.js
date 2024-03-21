e921582434eb1e0efe35aa25ceeaea2400e2c63d
NazarovAbdulboriy <nazarovabdulboriy377@gmail.com>
2024-03-21 09:34:37 +0500

    I add index.html and main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+let name1 = prompt("Ismingizni kiriting!")
+let letter = prompt("Istagan harfni kiriting!")
+
+let check = name1.toLowerCase().includes(letter.toLowerCase());
+
+if (check) {
+    alert("Siz kirtgan harf mavjud")
+} else (
+    alert("Siz kiritgan harf mavjud emas..")
+)
